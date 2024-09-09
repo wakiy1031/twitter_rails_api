ca9793ef686025f59e5d36031ef411923f8822cb
wakiy1031 <wakiy1031@gmail.com>
2024-09-10 07:26:32 +0900

    新規登録のコントローラー作成

---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+        private
+
+        def sign_up_params
+          params.permit(:name, :email, :phone, :birthdate, :password, :password_confirmation)
+        end
+      end
+    end
+  end
+end
