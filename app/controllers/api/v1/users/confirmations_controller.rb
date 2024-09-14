3b3693d9cbfdc8af15f364058461c7a9024b3a1c
wakiy1031 <wakiy1031@gmail.com>
2024-09-14 20:48:57 +0900

    rails-i18nとconfirmationの設定

---
+++ app/controllers/api/v1/users/confirmations_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  module V1
+    module Users
+      class ConfirmationsController < DeviseTokenAuth::ConfirmationsController
+      end
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
+  validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/ }
+  validates :phone, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
+  validates :birthdate, presence: true
 end
