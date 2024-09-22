eff5de2e165b7114fe19e6422fdc7df26def4ec3
wakiy1031 <wakiy1031@gmail.com>
2024-09-23 08:56:00 +0900

    デフォルト値とNOT NULL制約を設定

--- db/migrate/20240909124634_devise_token_auth_create_users.rb
+++ db/migrate/20240909124634_devise_token_auth_create_users.rb
@@ -8,7 +8,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       t.string :encrypted_password, null: false, default: ''
       t.string :reset_password_token
       t.datetime :reset_password_sent_at
-      t.boolean :allow_password_change, default: false
+      t.boolean :allow_password_change, null: false, default: false
       t.datetime :remember_created_at
       t.string :confirmation_token
       t.datetime :confirmed_at
