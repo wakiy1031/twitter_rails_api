ff6dbaabe5cc2938033fcc8aec93b5161043e273
wakiy1031 <wakiy1031@gmail.com>
2024-11-26 17:59:32 +0900

    パラメータを許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::API
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: %i[name email phone birthdate password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :description, :place, :website, :user_name,
+      :avatar_image, :header_image
+    ])
   end
 end
