845e3e6349db9f6ce6db5b23fece7d3cacbe3b40
wakiy1031 <wakiy1031@gmail.com>
2024-12-21 07:34:28 +0900

    テキストの更新を可能

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,32 @@ module Api
       def show
         user = User.find(params[:id])
         render json: {
+          name: user.name,
+          email: user.email,
+          user_name: user.user_name,
+          place: user.place,
+          description: user.description,
+          website: user.website,
+          id: user.id,
+          posts_count: user.posts.count,
           tweets: user.posts.order(created_at: :desc)
         }, status: :ok
       rescue ActiveRecord::RecordNotFound
         render json: { error: 'ユーザーが見つかりません' }, status: :not_found
+      end
+
+      def update_profile
+        if current_api_v1_user.update(user_params)
+          render json: current_api_v1_user
+        else
+          render json: { errors: current_api_v1_user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :user_name, :description, :place, :website)
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,11 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/ }
   validates :phone, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
   validates :birthdate, presence: true
-  validates :user_name, presence: true, uniqueness: true
+  validates :website, format: { with: URI::DEFAULT_PARSER.make_regexp }, allow_blank: true
 
   has_many :posts, dependent: :destroy
   has_one_attached :avatar_image
   has_one_attached :header_image
 
-  before_validation :set_default_user_name, on: :create
 
-  private
-
-  def set_default_user_name
-    self.user_name = name if user_name.blank?
-  end
 end
